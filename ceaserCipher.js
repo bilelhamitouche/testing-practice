e86b38a411e9172915dcffbdea30ba48ac27745b
bilelhamitouche <bilelhamitouche4@gmail.com>
2024-05-05 06:25:26 +0100

    adding tests for ceaserCipher wrapping and case

--- ceaserCipher.js
+++ ceaserCipher.js
@@ -1,16 +1,62 @@
 function shift(char, shift) {
+  if (char === ' ') return char;
   return String.fromCharCode(char.charCodeAt(0) + (shift % 26));
 }
 
+function upperShift(char, shift, upperCase = false) {
+  if (char === ' ') return char;
+  if (upperCase) {
+    return String.fromCharCode(
+      'A'.charCodeAt(0) +
+        (shift % 26) -
+        ('Z'.charCodeAt(0) - char.charCodeAt(0) + 1),
+    );
+  }
+  return String.fromCharCode(
+    'a'.charCodeAt(0) +
+      (shift % 26) -
+      ('z'.charCodeAt(0) - char.charCodeAt(0) + 1),
+  );
+}
+
+function lowerShift(char, shift, upperCase = false) {
+  if (char === ' ') return char;
+  if (upperCase) {
+    return String.fromCharCode(
+      'Z'.charCodeAt(0) +
+        (shift % 26) +
+        (char.charCodeAt(0) - 'A'.charCodeAt(0) + 1),
+    );
+  }
+  return String.fromCharCode(
+    'z'.charCodeAt(0) +
+      (shift % 26) +
+      (char.charCodeAt(0) - 'a'.charCodeAt(0) + 1),
+  );
+}
+
 function ceaserCipher(str, key) {
-  let result = "";
+  let result = '';
   for (let i = 0; i < str.length; i++) {
-    result += shift(str[i], key);
+    if (str[i] === str[i].toUpperCase()) {
+      if (str[i].charCodeAt(0) + key < 65) {
+        result += lowerShift(str[i], key, true);
+      } else if (str[i].charCodeAt(0) + key > 90) {
+        result += upperShift(str[i], key, true);
+      } else {
+        result += shift(str[i], key);
+      }
+    } else if (str[i] === str[i].toLowerCase()) {
+      if (str[i].charCodeAt(0) + key < 97) {
+        result += lowerShift(str[i], key);
+      } else if (str[i].charCodeAt(0) + key > 122) {
+        result += upperShift(str[i], key);
+      } else {
+        result += shift(str[i], key);
+      }
+    }
   }
   return result;
 }
 
-//export default ceaserCipher;
-
-console.log(ceaserCipher("bilel0", 1));
-console.log(ceaserCipher("Bilel", 4));
+export default ceaserCipher;
--- ceaserCipher.test.js
+++ ceaserCipher.test.js
@@ -1,9 +1,14 @@
-import ceaserCipher from "./ceaserCipher";
+import ceaserCipher from './ceaserCipher';
 
-test("ceaserCipher exists", () => {
+test('ceaserCipher exists', () => {
   expect(ceaserCipher).toBeDefined();
 });
 
-test("testCases for ceaserCipher", () => {
-  expect(ceaserCipher("rachid", 2)).toEqual("tcejkf");
+test('testCases for ceaserCipher', () => {
+  expect(ceaserCipher('', 2)).toEqual('');
+  expect(ceaserCipher('rachid', 2)).toEqual('tcejkf');
+  expect(ceaserCipher('rachid rachid', 2)).toEqual('tcejkf tcejkf');
+  expect(ceaserCipher('yyy', -1)).toEqual('xxx');
+  expect(ceaserCipher('Ed', -8)).toEqual('Wv');
+  expect(ceaserCipher('Zy', 4)).toEqual('Dc');
 });
